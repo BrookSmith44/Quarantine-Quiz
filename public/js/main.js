f1c706c3bbea839ef26b8c2cb60f3fc9bed51c81
BrookSmith44 <brook.smith.182@gmail.com>
2020-06-05 13:56:32 +0100

    Get values of Round form

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ const chatForm = document.getElementById('chat-form');
 // Get ready form by ID
 const readyForm = document.getElementById('ready-form');
 // Get create round form by ID
-const createRoundForm = document.getElementById('create-form');
+const btnCreateRound = document.getElementById('create-form');
 // Get chat main by ID
 const chatMain = document.getElementById('chat-main');
 // Get chat container by class
@@ -22,10 +22,24 @@ const createRoundContainer = document.getElementById('create-container');
 const addQDiv = document.getElementById('additional-question');
 // Get add question button
 const btnAdd = document.getElementById('btnAdd');
+// get create form by id
+const createRoundForm = document.getElementById('create-round');
+// Get answer options div by class
+const ansOptions = document.querySelector('.ans-options');
+// get answer radio by ID
+const ansRadio  = document.getElementById('answer');
+// const get form
 // Get username from URL
  //let params = new URLSearchParams(location.search);
  //const username = params.get('username');
+ // Set empty username variable
 let username= '';
+// Set num to 2
+let num = 2;
+// Set answer number to 0
+let ansNum = 1;
+// Number of questions
+let QNum = 1;
 
 
 
@@ -42,7 +56,6 @@ userForm.addEventListener('submit', (e) => {
 });
 
 function CreateRound() {
-    let num = 2;
     // hide user and chat containers and display create round container
     userContainer.style.display = 'none';
     chatContainer.style.display = 'none';
@@ -50,8 +63,69 @@ function CreateRound() {
     createRoundContainer.style.display = 'flex';
 
     btnAdd.addEventListener('click', () => {
+        // Function to add question inputs into form
         addQuestion(num);
+        // Increment num
+        num++;
     });
+
+    createRoundForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        roundArr = [];
+        // Loop through all questions
+        for (i = 1; i <= QNum; i++) {
+            // Loop through question input
+            const questionInput = document.getElementById(`questionInput-${i}`);
+            // Loop through marks input
+            const marksInput = document.getElementById(`marks-${i}`);
+            // Loop through answer options
+            const voteRadio = document.getElementById(`vote-${i}`);
+            const answerRadio = document.getElementById(`answer-${i}`);
+            const choiceRadio = document.getElementById(`choice-${i}`);
+            
+            // Get question text
+            const question = questionInput.value;
+            // get mark number
+            const mark = marksInput.value;
+            // empty varirable for type
+            let type = '';
+            // empty varirable for answer
+            let answer = '';
+
+            // If vote is checked
+            if (voteRadio.checked) {
+                // set type to vote
+                 type = voteRadio.value;
+            } 
+            // Else if answer is checked
+            else if(answerRadio.checked) {
+                // set type to answer
+                 type = answerRadio.value;
+                 const answerInput = document.getElementById(`answer-input-${i}`);
+                answer = answerInput.value;
+            } 
+            // Else if choice is checked
+            else if(choiceRadio.checked) {
+                // set type to choice
+                 type = choiceRadio.value;
+            }
+
+            // Set object for each question
+            const questionObj = {
+                question: question,
+                mark: mark,
+                answerObj: {
+                    answer: answer,
+                    type: type
+                }
+            }
+
+            // Push object into array
+            roundArr.push(questionObj);
+        }
+        console.log(roundArr);
+    });
+
 }
 
 // Function to set up lobby
@@ -110,7 +184,7 @@ function userLobby()
     });
 
     // Create round button
-    createRoundForm.addEventListener('submit', (e) => {
+    btnCreateRound.addEventListener('submit', (e) => {
         e.preventDefault();
 
         CreateRound();
@@ -177,24 +251,122 @@ function addQuestion(num) {
     questionDiv.innerHTML =`
     <div id="question-${num}" class="questions">
                 <div class="question-input">
-                    <label for="question"><h3>Question: ${num}</h3></label><input id="question" type="text" placeholder="Write Question"/>
+                    <label for="question"><h3>Question: ${num}</h3></label><input class="questionInput" id="questionInput-${num}" type="text" placeholder="Write Question"/>
                 </div>
                 <label for="imgUpload"><h3>Upload Image (If Needed)</h3></label><input id="imgUpload" type='file' />
-                <div class="marks">
-                    <label for="marks"><h3>Marks: </h3></label><input id="marks" type="number" min="1" />
+                <div class="marksContainer">
+                    <label for="marks"><h3>Marks: </h3></label><input class="marks" id="marks-${num}" type="number" min="1" />
                 </div>
-                <div class="asn-options">
+                <div id="options-${num}" class="ans-options">
                     <label><h3>Answer Options: </h3></label>
-                    <div id="vote-div" class="option"><label for="vote">Vote</label><input id="vote" class="answer" type="radio" name="ans-option" value="vote" /></div>
-                    <div id="answer-div" class="option"><label for="answer">Answer</label><input id="answer" class="answer" type="radio" name="ans-option" value="answer" /></div>
-                    <div id="choice-div" class="option"><label for="choice">Multiple Choice</label><input id="choice" class="answer" type="radio" name="ans-option" value="choice" /></div>
+                    <div id="vote-div" class="option"><label for="vote">Vote</label><input id="vote-${num}" class="answer" type="radio" name="ans-option-${num}" value="vote" /></div>
+                    <div id="answer-div" class="option"><label for="answer">Answer</label><input id="answer-${num}" class="answer" type="radio" name="ans-option-${num}" value="answer" onchange="createAnswerInput(this);" /></div>
+                    <div id="choice-div" class="option"><label for="choice">Multiple Choice</label><input id="choice-${num}" class="answer" type="radio" name="ans-option-${num}" value="choice" onchange="createNumOfChoices(this);" /></div>
                 </div>  
             </div>
     `;
     // append div from form
     addQDiv.appendChild(questionDiv);
 
-    // Increment num
-    num++;
-    console.log(num);
+    // Increment QNum
+    QNum++;
 }
+
+// Answer input 
+function createAnswerInput(element) {
+    // Get parent element by ID
+    const ansOptionParent = document.getElementById(element.parentNode.parentNode.id);
+    // Get answer input by class name
+    const choiceInput = ansOptionParent.querySelector('.numOfChoice');
+    // Get answer input by class name
+    const ansInput = ansOptionParent.querySelector('.answer-input');
+    // Get choices div
+    const choicesDiv = ansOptionParent.querySelector('.choices-div');
+    console.log(ansOptionParent.childNodes);
+    // If choice input exists
+    if(choiceInput) {
+        choicesDiv.remove();
+    }
+    if(!ansInput) {
+        // Create new div
+        const div = document.createElement('Div');
+        // Add class to div
+        div.classList.add('answer-container');
+        // Set id for div
+        div.setAttribute('id', `answer-${QNum}`);
+        // Set content of div
+        div.innerHTML = `
+        <input id="answer-input-${QNum}" class="answer-input" type="text" placeholder="Write Answer" />
+        `;
+        // Append div from ans-options div
+        ansOptionParent.appendChild(div);   
+    }
+}
+
+// choice input
+function createNumOfChoices(element) {
+    // Get parent element by ID
+     const ansOptionParent = document.getElementById(element.parentNode.parentNode.id);
+    // Get answer input by class name
+    const ansInput = ansOptionParent.querySelector('.answer-input');
+    // Get answer input by class name
+    const choiceInput = ansOptionParent.querySelector('.numOfChoice');
+    // If the answer input exists
+    if(ansInput) {
+        // Hide answer input
+        ansInput.remove();
+    }
+    // If number of choice input does not exist
+    if(!choiceInput) {
+        // Create new div
+        const div = document.createElement('Div');
+        // Add class to div
+        div.classList.add('choices-div');
+        // Set content of div
+        div.innerHTML = `
+        <input class="numOfChoice" type="number" value="0" min="1" onchange="numOfAnswers(this);" />
+        `;
+        // Append div from ans-options div
+        ansOptionParent.appendChild(div); 
+    } 
+}
+    
+    function numOfAnswers(element) {
+        // Get parent element by ID
+        const ansOptionParent = document.getElementById(element.parentNode.parentNode.id);
+        console.log(ansOptionParent);
+        // Get answer input by class name
+        const choiceInput = ansOptionParent.querySelector('.numOfChoice');
+        // Get div for choices 
+        const choicesDiv = ansOptionParent.querySelector('.choices-div');
+        let choiceValue = choiceInput.value;
+        choiceValue++;
+        if (choiceValue > ansNum) {
+            // for loop to create answer inpu
+            for (ansNum; ansNum < choiceValue; ansNum++) {
+            // Create new div
+            const div = document.createElement('Div');
+            // Create class for div
+            div.classList.add('choices');
+            // Create is for div
+            div.setAttribute('id', `choices-${ansNum}`)
+            // Set content of div
+            div.innerHTML = `
+            <label for="choice-input"><h3>Choice ${ansNum}: </h3></label><input id="choice-${ansNum}" class="answer-input" type="text" placeholder="Write Answer" />
+            `;
+            // Append div from ans-option
+            // Append div from ans-options div
+            choicesDiv.appendChild(div); 
+            console.log(ansNum);
+            }
+        } else if (choiceValue < ansNum) {
+            console.log(`answer-${ansNum-1}`);
+            for (ansNum; ansNum > choiceValue; ansNum--) {
+                // Get div by ID
+                const div = document.getElementById(`answer-${ansNum-1}`);
+                console.log(div);
+                // remove div
+                div.remove();
+            }
+        }
+    }
